b3d8f6bbe4efd29b3bfb9b889ecef2f59dd1651f
David Galleguillos <galleguillos.david@gmail.comglobal user.email galleguillos.david@gmail.com>
2020-10-20 22:24:36 -0300

    7

--- app/src/main/java/com/example/intento7/Model/Repository/SuperHeroesRepository.kt
+++ app/src/main/java/com/example/intento7/Model/Repository/SuperHeroesRepository.kt
@@ -3,13 +3,20 @@ package com.example.intento7.Model.Repository
 import android.util.Log
 import com.example.intento7.Model.NetworkWithRetrofit.RetrofitClient
 import com.example.intento7.Model.NetworkWithRetrofit.SuperHeroes
+import com.example.intento7.Model.RoomLocal.SuperHeroesDao
+import com.example.intento7.Model.RoomLocal.SuperHeroesEntity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SuperHeroesRepository() {
-private val retroService = RetrofitClient.getRetrofitClient()
+class SuperHeroesRepository(private val superHeroesDao: SuperHeroesDao) {
 
+    private val retroService = RetrofitClient.getRetrofitClient()
+    // Este muestra toda la tabla de super heroes entity
+    val allSuperHeroesLiveData = superHeroesDao.showAllSuperHeroes()
 //    La vieja confiable
 
     fun getDataFromServer() {
@@ -21,6 +28,11 @@ private val retroService = RetrofitClient.getRetrofitClient()
             ) {
                 when(response.code()){
                     in 200..299 -> Log.d("acierto",response.body().toString())
+                    CoroutineScope(Dispatchers.IO).launch {
+                        response.body()?.let {
+                            superHeroesDao.insertAllSuperHeroes(convert(it))
+                        }
+                    }
                     in 300..399 -> Log.d("acierto",response.body().toString())
                     in 400..499 -> Log.d("acierto",response.body().toString())
                     in 500..599 -> Log.d("acierto",response.body().toString())
@@ -34,5 +46,22 @@ private val retroService = RetrofitClient.getRetrofitClient()
                 Log.e("error", t.message.toString())
             }
         })
+    }
+
+    fun convert(listFromNetwork: List<SuperHeroes>): List<SuperHeroesEntity> {
+        val listMutable = mutableListOf<SuperHeroesEntity>()
+        listFromNetwork.map {
+            listMutable.add(
+                SuperHeroesEntity(
+                    it.id,
+                    it.images.xs,
+                    it.images.lg,
+                    it.name,
+                    it.biography.alterEgos,
+                    it.appearance.height
+                )
+            )
+        }
+        return listMutable
     }
 }
--- app/src/main/java/com/example/intento7/Model/ViewModel/SuperHeroesViewodel.kt
+++ app/src/main/java/com/example/intento7/Model/ViewModel/SuperHeroesViewodel.kt
@@ -1,12 +1,21 @@
 package com.example.intento7.Model.ViewModel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.intento7.Model.Repository.SuperHeroesRepository
+import com.example.intento7.Model.RoomLocal.SuperHeroesDataBase
+import com.example.intento7.Model.RoomLocal.SuperHeroesEntity
 
-class SuperHeroesViewodel: ViewModel() {
+class SuperHeroesViewodel(application: Application): AndroidViewModel(application) {
     private val mRepository: SuperHeroesRepository
+    val liveDataFromLocal : LiveData<List<SuperHeroesEntity>>
 
     init {
-        mRepository = SuperHeroesRepository()
+        val superHeroesDao = SuperHeroesDataBase.getDatabase(application).superHeroesDao()
+        mRepository = SuperHeroesRepository(superHeroesDao)
+        mRepository.getDataFromServer()
+        liveDataFromLocal = mRepository.allSuperHeroesLiveData
     }
 }
