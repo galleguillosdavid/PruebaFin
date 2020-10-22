86728d57402934f2feb34f131ed6e12fd24731f0
galleguillosdavid <galleguillos.david@gmail.com>
2020-10-22 09:29:08 -0300

    9

--- app/src/main/java/com/example/intento7/ui/FirstFragment.kt
+++ app/src/main/java/com/example/intento7/ui/FirstFragment.kt
@@ -10,17 +10,28 @@ import android.widget.Button
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.intento7.viewModel.SuperHeroesViewModel
 import com.example.intento7.R
+import com.example.intento7.model.recyclerView.PassTheData
+import com.example.intento7.model.recyclerView.SuperHeroesAdapter
+import com.example.intento7.model.repository.roomLocal.SuperHeroesEntity
+import kotlinx.android.synthetic.main.fragment_first.*
 
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), PassTheData {
 
     private val model: SuperHeroesViewModel by activityViewModels()
+    lateinit var mAdapter: SuperHeroesAdapter
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mAdapter = SuperHeroesAdapter(this)
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -33,13 +44,22 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        model.liveDataFromLocal.observe(viewLifecycleOwner, Observer {
-            Log.d("View",it.toString()) })
 
+        val recyclerView = RecyclerView1
+        recyclerView.adapter = mAdapter
+        recyclerView.layoutManager = GridLayoutManager(context,2)
+        model.liveDataFromLocal.observe(viewLifecycleOwner, Observer {
+            Log.d("View", it.toString())
+            mAdapter.updateAdapter(it)
+        })
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+    }
+
+    override fun passTheSuperHeroes(superHeroes: SuperHeroesEntity) {
+        TODO("Not yet implemented")
     }
 
 }
