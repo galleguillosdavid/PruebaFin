d37e04b58bf49baaf2c77ca5ec320bbfc65bf0d1
galleguillosdavid <galleguillos.david@gmail.com>
2020-11-06 15:02:12 -0300

    Full View

--- app/src/main/java/com/example/intento7/ui/SecondFragment.kt
+++ app/src/main/java/com/example/intento7/ui/SecondFragment.kt
@@ -12,8 +12,8 @@ import com.bumptech.glide.request.RequestOptions
 import com.example.intento7.R
 import com.example.intento7.viewModel.SuperHeroesViewModel
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_second.*
+import kotlinx.android.synthetic.main.fragment_second.textView1
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -48,30 +48,30 @@ class SecondFragment : Fragment() {
             model.getSuperHeroesByid(superheroesId).observe(viewLifecycleOwner, androidx.lifecycle.Observer {
                 if (it != null) {
                     textView.text = it.name
-                    textView1.text = it.id.toString()
-                    textView2.text = it.fullName
-                    textView3.text =  it.alterEgos
-                    textView4.text =  it.firstAppearance
-                    textView5.text =  it.gender
-                    textView6.text =  it.alignment
-                    textView7.text =  it.base
-                    textView8.text =  it.eyeColor
-                    textView9.text =  it.groupAffiliation
-                    textView10.text =  it.hairColor
-                    textView11.text =  it.occupation
-                    textView12.text =  it.placeOfBirth
-                    textView13.text =  it.publisher
-                    textView14.text =  it.race
-                    textView15.text =  it.relatives
-                    textView16.text =  it.slug
-                    textView17.text =  it.slug
-                    textView18.text =  it.slug
-                    textView19.text =  it.slug
-                    textView20.text =  it.slug
+                    textView1.text = "Code " + it.id.toString()
+                    textView2.text = "Full Name " + it.fullName
+                    textView3.text = "Alter Egos " +  it.alterEgos
+                    textView4.text = "First Appearance " +  it.firstAppearance
+                    textView5.text = "Gender " +  it.gender
+                    textView6.text = "Full Name " +  it.alignment
+                    textView7.text = "Alignment " +  it.base
+                    textView8.text = "Eye Color " +  it.eyeColor
+                    textView9.text = "Group Affiliation " +  it.groupAffiliation
+                    textView10.text = "Hair Color " +  it.hairColor
+                    textView11.text = "Occupation " +  it.occupation
+                    textView12.text = "Place Of Birth " +  it.placeOfBirth
+                    textView13.text = "Publisher " +  it.publisher
+                    textView14.text = "Race " +  it.race
+                    textView15.text = "Relatives " +  it.relatives
+                    textView16.text = "Slug " +  it.slug
+                    textView17.text = "Xs " +  it.xs
+                    textView18.text = "Sm " +  it.sm
+                    textView19.text = "Md " +  it.md
+                    textView20.text = "Lg " +  it.lg
 
-                    val xs = Glide.with(this).load(it.xs).apply(RequestOptions.circleCropTransform()).into(imageView2)
-                    val sm = Glide.with(this).load(it.sm).apply(RequestOptions.circleCropTransform()).into(imageView2)
-                    val md = Glide.with(this).load(it.md).apply(RequestOptions.circleCropTransform()).into(imageView2)
+//                    val xs = Glide.with(this).load(it.xs).apply(RequestOptions.circleCropTransform()).into(imageView2)
+//                    val sm = Glide.with(this).load(it.sm).apply(RequestOptions.circleCropTransform()).into(imageView2)
+//                    val md = Glide.with(this).load(it.md).apply(RequestOptions.circleCropTransform()).into(imageView2)
                     val lg = Glide.with(this).load(it.lg).apply(RequestOptions.circleCropTransform()).into(imageView2)
                 }
             })
